e11380bbfbbb9641093a86b40a7abb69d8665cc6
Prakash Manglanathan <pmnathan@gmail.com>
2021-01-14 13:44:37 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,6 +21,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
+  count         = 3
 
   tags = {
     Name = "HelloWorld"

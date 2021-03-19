d090538cddea7922079fedb89c3b6a7c3c7270e4
Prakash Manglanathan <pmnathan@gmail.com>
2021-03-19 12:51:54 -0400

    Update main.tf
    Prakash I don't think 2 instances will be enough..so changing it to 4

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.small"
-  count         = 2
+  count         = 4
 
   tags = {
     Name = "workshop_demo_2021"

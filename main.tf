fff868d1cdede40d17e6b1057a2d0eb2a0c43ff4
Prakash Manglanathan <pmnathan@gmail.com>
2021-04-20 09:19:09 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,68 @@ provider "aws" {
   region = "us-west-2"
 }
 
+resource "aws_vpc" "hashi" {
+  cidr_block           = var.address_space
+  enable_dns_hostnames = true
+
+  tags = {
+    name = "${var.prefix}-vpc-${var.region}"
+  }
+}
+
+
+
+ resource "aws_subnet" "hashi" {
+   vpc_id     = aws_vpc.hashi.id
+   cidr_block = var.subnet_prefix
+
+   tags = {
+     name = "${var.prefix}-subnet"
+   }
+ }
+
+
+
+ resource "aws_security_group" "hashi" {
+   name = "${var.prefix}-security-group"
+
+   vpc_id = aws_vpc.hashi.id
+
+   ingress {
+     from_port   = 22
+     to_port     = 22
+     protocol    = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
+
+   ingress {
+     from_port   = 80
+     to_port     = 80
+     protocol    = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
+
+   ingress {
+     from_port   = 443
+     to_port     = 443
+     protocol    = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
+
+   egress {
+     from_port       = 0
+     to_port         = 0
+     protocol        = "-1"
+     cidr_blocks     = ["0.0.0.0/0"]
+     prefix_list_ids = []
+   }
+
+   tags = {
+     Name = "${var.prefix}-security-group"
+   }
+}
+
+
 data "aws_ami" "ubuntu" {
   most_recent = true
 
@@ -18,10 +80,13 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.small"
-  count         = 3
+  ami                     = data.aws_ami.ubuntu.id
+  subnet_id               = aws_subnet.hashi.id
+  vpc_security_group_ids  = [aws_security_group.hashi.id]
+  instance_type           = "t2.micro"
+  count                   = 3
 
   tags = {
     Name = "demo_2021_${count.index}"
@@ -30,7 +95,6 @@ resource "aws_instance" "web" {
 
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
-
   bucket = "my-s3-bucket-prakash-2021"
   acl    = "private"
 

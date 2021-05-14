cad26c8d04508486984518be1acf273ee1f41b85
Prakash Manglanathan <pmnathan@gmail.com>
2021-05-13 22:52:29 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -93,13 +93,12 @@ resource "aws_instance" "web" {
   }
 }
 
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-  bucket = "my-s3-bucket-prakash-2021"
-  acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
-
-}
+#module "s3_bucket" {
+#  source = "terraform-aws-modules/s3-bucket/aws"
+#  bucket = "my-s3-bucket-prakash-2021"
+#  acl    = "private"
+#
+#  versioning = {
+#    enabled = true
+#  }
+#}

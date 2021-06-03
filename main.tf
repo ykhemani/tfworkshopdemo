3835e5ba0aad21080f17b32d2c198d5d89a68a07
Prakash Manglanathan <pmnathan@gmail.com>
2021-06-03 09:19:30 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -86,11 +86,11 @@ resource "aws_instance" "web" {
   subnet_id               = aws_subnet.hashi.id
   vpc_security_group_ids  = [aws_security_group.hashi.id]
   instance_type           = "t2.small"
-  count                   = 5
+  count                   = 3
 
   tags = {
     Name = "demo_2021_${count.index}"
-    Project = "FEPOC_Main"
+    Project = "workshop_Main"
     Environment = "Development"
   }
 }

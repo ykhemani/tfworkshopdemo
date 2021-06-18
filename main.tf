7e802b052f6332969b185fb52d312e26f5b362ae
Prakash Manglanathan <pmnathan@gmail.com>
2021-06-18 12:17:59 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -82,7 +82,7 @@ resource "aws_instance" "web" {
   ami                     = data.aws_ami.ubuntu.id
   subnet_id               = aws_subnet.hashi.id
   vpc_security_group_ids  = [aws_security_group.hashi.id]
-  instance_type           = "t2.micro"
+  instance_type           = "t2.small"
   count                   = 5
 
   tags = {

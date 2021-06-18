f60ee9e4d216d45730b657c8c5f1c48dea7ff0b3
Prakash Manglanathan <pmnathan@gmail.com>
2021-06-18 12:47:53 -0400

    changed to t2.medium

--- main.tf
+++ main.tf
@@ -82,7 +82,7 @@ resource "aws_instance" "web" {
   ami                     = data.aws_ami.ubuntu.id
   subnet_id               = aws_subnet.hashi.id
   vpc_security_group_ids  = [aws_security_group.hashi.id]
-  instance_type           = "t2.small"
+  instance_type           = "t2.medium"
   count                   = 5
 
   tags = {

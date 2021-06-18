34a5c356f01353833645a4c50a6d3e7f2c55d80e
pmnathandev2 <82589430+pmnathandev2@users.noreply.github.com>
2021-06-18 12:30:12 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -83,10 +83,10 @@ resource "aws_instance" "web" {
   subnet_id               = aws_subnet.hashi.id
   vpc_security_group_ids  = [aws_security_group.hashi.id]
   instance_type           = "t2.small"
-  count                   = 3
+  count                   = 5
 
   tags = {
-    Name = "demo_2021_${count.index}"
+    Name = "demo_2021_53bank_${count.index}"
     Project = "workshop_Main"
     Environment = "Development"
     importid = "FN20210001"

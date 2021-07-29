4504352298f4b594adbca0b59a0299b48c441898
Prakash Manglanathan <pmnathan@gmail.com>
2021-07-29 17:03:18 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -88,8 +88,8 @@ resource "aws_instance" "web" {
   ami                     = data.aws_ami.ubuntu.id
   subnet_id               = aws_subnet.hashi.id
   vpc_security_group_ids  = [aws_security_group.hashi.id]
-  instance_type           = "t2.2xlarge"
-  count                   = 4
+  instance_type           = "t2.medium"
+  count                   = 8
 
   tags = {
     Name = "${var.environment}_demo_2021_${count.index}"

87aeed7a56de37c383e12f9430a0ac1d7ba55761
Prakash Manglanathan <pmnathan@gmail.com>
2022-03-10 15:22:59 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -89,7 +89,7 @@ resource "aws_instance" "web" {
   subnet_id               = aws_subnet.hashi.id
   vpc_security_group_ids  = [aws_security_group.hashi.id]
   instance_type           = "t2.small"
-  count                   = 1
+  count                   = 5
 
   tags = {
     Name = "${var.environment}_demo_2021_${count.index}"

dea269056171b8c8a5fcb721dd379495f53f65eb
Prakash Manglanathan <pmnathan@gmail.com>
2022-07-25 09:59:23 -0400

    added keypair and public IP

--- main.tf
+++ main.tf
@@ -97,8 +97,10 @@ resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
   subnet_id              = aws_subnet.hashi.id
   vpc_security_group_ids = [aws_security_group.hashi.id]
-  instance_type          = "t2.2xlarge"
-  count                  =  6
+  instance_type          = "t2.small"
+  key_name               = "Prakash-demo"
+  associate_public_ip_address  = true
+  count                  =  2
 
   tags = {
     Name     = "${var.environment}_demo_2022_${count.index}"

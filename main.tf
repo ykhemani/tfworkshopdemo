83fa29d73832c991f3434d1c4eb8950bfbe5a059
Prakash Manglanathan <pmnathan@gmail.com>
2023-03-10 09:08:21 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -100,7 +100,7 @@ resource "aws_instance" "web" {
   instance_type          = "t2.small"
   #key_name               = "Prakash-demo"
   #associate_public_ip_address  = true
-  count                  =  6
+  count                  =  2
 
   tags = {
     Name     = "Anthem-workshop-demo"

3c3040d270b414a4d9dda5eece8be172865089f0
Prakash Manglanathan <pmnathan@gmail.com>
2023-03-09 21:38:21 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -98,8 +98,8 @@ resource "aws_instance" "web" {
   subnet_id              = aws_subnet.hashi.id
   vpc_security_group_ids = [aws_security_group.hashi.id]
   instance_type          = "t2.small"
-  key_name               = "Prakash-demo"
-  associate_public_ip_address  = true
+  #key_name               = "Prakash-demo"
+  #associate_public_ip_address  = true
   count                  =  1
 
   tags = {

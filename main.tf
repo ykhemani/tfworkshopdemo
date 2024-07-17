974a2b94a01bbf3e0a7a6f65415ab17de5faf58e
Prakash Manglanathan <pmnathan@gmail.com>
2024-07-17 12:54:23 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -97,10 +97,10 @@ resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
   subnet_id              = aws_subnet.hashi.id
   vpc_security_group_ids = [aws_security_group.hashi.id]
-  instance_type          = "t2.small"
+  instance_type          = "t2.medium"
   #key_name               = "Prakash-demo"
   #associate_public_ip_address  = true
-  count                  =  5
+  count                  =  2
 
   tags = {
     Name     = "hashi-workshop-demo"
